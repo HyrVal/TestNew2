a0e37ff63143ff508805e28ae81cff9927b0148a
HyrVal <val.hyrenko@gmail.com>
2022-07-04 11:05:15 +0300

    Added autotests for math operation

--- src/test/java/TestMathOper.java
+++ src/test/java/TestMathOper.java
@@ -4,34 +4,38 @@ import org.testng.annotations.Test;
 
 public class TestMathOper {
 
-        MathOperSum mathOperSum = new MathOperSum();
-        @Test
-        public void chekOperSum2Equals (){
-            Assert.assertEquals(mathOperSum.doItSum(2,3) , 5);
-            Assert.assertEquals(mathOperSum.doItSum(3,4) , 7);
-        }
-        @Test
-        public void chekOperSum2True(){
-            Assert.assertEquals(mathOperSum.doItSum(2, 3), 5);
-        }
-        @Test
-        public void chekOperSum2False(){
-            Assert.assertNotEquals(mathOperSum.doItSum(2, 3), 6);
-        }
+    MathOperSum mathOperSum = new MathOperSum();
+
     @Test
-    public void chekOperSum3Equals (){
-        Assert.assertEquals(mathOperSum.doItSum(2,3, 4) , 9);
-        Assert.assertEquals(mathOperSum.doItSum(3,4, 1) , 8);
-    }
-    @Test
-    public void chekOperSum3True(){
-        Assert.assertEquals(mathOperSum.doItSum(2, 3,4), 9);
-    }
-    @Test
-    public void chekOperSum3False(){
-        Assert.assertNotEquals(mathOperSum.doItSum(2, 3,4), 6);
+    public void chekOperSum2Equals() {
+        Assert.assertEquals(mathOperSum.doItSum(2, 3), 5);
+        Assert.assertEquals(mathOperSum.doItSum(3, 4), 7);
     }
 
-
+    @Test
+    public void chekOperSum2True() {
+        Assert.assertEquals(mathOperSum.doItSum(2, 3), 5);
     }
+
+    @Test
+    public void chekOperSum2False() {
+        Assert.assertNotEquals(mathOperSum.doItSum(2, 3), 6);
+    }
+
+    @Test
+    public void chekOperSum3Equals() {
+        Assert.assertEquals(mathOperSum.doItSum(2, 3, 4), 9);
+        Assert.assertEquals(mathOperSum.doItSum(3, 4, 1), 8);
+    }
+
+    @Test
+    public void chekOperSum3True() {
+        Assert.assertEquals(mathOperSum.doItSum(2, 3, 4), 9);
+    }
+
+    @Test
+    public void chekOperSum3False() {
+        Assert.assertNotEquals(mathOperSum.doItSum(2, 3, 4), 6);
+    }
+}
 
